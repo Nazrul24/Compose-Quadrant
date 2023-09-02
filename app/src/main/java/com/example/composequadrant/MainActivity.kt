69d3c2e70e67b738c5778ecc2d344cfb59f92fc9
Kazi Nazrul Islam Hridoy <kaziislam2484@gmail.com>
2023-09-03 00:14:38 +0600

    Divides the screen into four equal parts using column and row

--- app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -3,18 +3,24 @@ package com.example.composequadrant
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
@@ -29,7 +35,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ComposeQuadrant();
+                    ComposeQuadrant()
                 }
             }
         }
@@ -38,8 +44,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ComposeQuadrant() {
-    Column {
-        Row {
+    Column(Modifier.fillMaxWidth()){
+        Row(Modifier.weight(1f)) {
             ComposableInfoCard(
                 title = stringResource(id = R.string.text_title),
                 description = stringResource(id = R.string.text_description),
@@ -55,7 +61,7 @@ fun ComposeQuadrant() {
             )
         }
 
-        Row {
+        Row (Modifier.weight(1f)){
             ComposableInfoCard(
                 title = stringResource(id = R.string.row_title),
                 description = stringResource(id = R.string.row_description),
@@ -81,14 +87,20 @@ private fun ComposableInfoCard(
 ){
     Column(
         modifier = modifier
-            .padding(16.dp)
-        
+            .fillMaxSize()
+            .background(backgroundColor),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = title
+            text = title,
+            modifier = Modifier.padding(bottom = 16.dp),
+            fontWeight = FontWeight.Bold
         )
         Text(
-            text = description
+            text = description,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(16.dp)
         )
     }
 
@@ -98,6 +110,6 @@ private fun ComposableInfoCard(
 @Composable
 fun GreetingPreview() {
     ComposeQuadrantTheme {
-        ComposeQuadrant();
+        ComposeQuadrant()
     }
 }
